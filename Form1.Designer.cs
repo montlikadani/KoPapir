1b5de0c5e048ff8eebababf2dd504aaa847d4e5b
montlikadani <montlikada@gmail.com>
2023-03-01 15:38:55 +0100

    half

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace StPap {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.firstSelection = new System.Windows.Forms.TextBox();
+            this.secondSelection = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eredmeny = new System.Windows.Forms.Label();
+            this.specsf = new System.Windows.Forms.Label();
+            this.tovabbiJatekok = new System.Windows.Forms.Label();
+            this.statsText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstSelection
+            // 
+            this.firstSelection.Location = new System.Drawing.Point(37, 49);
+            this.firstSelection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.firstSelection.Name = "firstSelection";
+            this.firstSelection.Size = new System.Drawing.Size(217, 22);
+            this.firstSelection.TabIndex = 0;
+            this.firstSelection.TextChanged += new System.EventHandler(this.Selection_TextChanged);
+            // 
+            // secondSelection
+            // 
+            this.secondSelection.Location = new System.Drawing.Point(578, 49);
+            this.secondSelection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.secondSelection.Name = "secondSelection";
+            this.secondSelection.Size = new System.Drawing.Size(217, 22);
+            this.secondSelection.TabIndex = 2;
+            this.secondSelection.TextChanged += new System.EventHandler(this.Selection_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1. játékos választása";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(575, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2. játékos választása";
+            // 
+            // eredmeny
+            // 
+            this.eredmeny.AutoSize = true;
+            this.eredmeny.Location = new System.Drawing.Point(345, 115);
+            this.eredmeny.Name = "eredmeny";
+            this.eredmeny.Size = new System.Drawing.Size(44, 16);
+            this.eredmeny.TabIndex = 5;
+            this.eredmeny.Text = "label3";
+            this.eredmeny.Visible = false;
+            // 
+            // specsf
+            // 
+            this.specsf.AutoSize = true;
+            this.specsf.Location = new System.Drawing.Point(350, 17);
+            this.specsf.Name = "specsf";
+            this.specsf.Size = new System.Drawing.Size(39, 16);
+            this.specsf.TabIndex = 6;
+            this.specsf.Text = "0 - kő";
+            // 
+            // tovabbiJatekok
+            // 
+            this.tovabbiJatekok.AutoSize = true;
+            this.tovabbiJatekok.Location = new System.Drawing.Point(345, 186);
+            this.tovabbiJatekok.Name = "tovabbiJatekok";
+            this.tovabbiJatekok.Size = new System.Drawing.Size(44, 16);
+            this.tovabbiJatekok.TabIndex = 7;
+            this.tovabbiJatekok.Text = "label3";
+            // 
+            // statsText
+            // 
+            this.statsText.AutoSize = true;
+            this.statsText.Location = new System.Drawing.Point(348, 240);
+            this.statsText.Name = "statsText";
+            this.statsText.Size = new System.Drawing.Size(44, 16);
+            this.statsText.TabIndex = 8;
+            this.statsText.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 339);
+            this.Controls.Add(this.statsText);
+            this.Controls.Add(this.tovabbiJatekok);
+            this.Controls.Add(this.specsf);
+            this.Controls.Add(this.eredmeny);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondSelection);
+            this.Controls.Add(this.firstSelection);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kő papír olló";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstSelection;
+        private System.Windows.Forms.TextBox secondSelection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label eredmeny;
+        private System.Windows.Forms.Label specsf;
+        private System.Windows.Forms.Label tovabbiJatekok;
+        private System.Windows.Forms.Label statsText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace StPap {
+    public partial class Form1 : Form {
+
+        private readonly List<PeopleSelection> peopleSelections;
+
+        public Form1() {
+            InitializeComponent();
+
+            string[] split = Properties.Resources.jatek.Split('\n');
+
+            peopleSelections = new List<PeopleSelection>(split.Length);
+
+            foreach (string one in split) {
+                foreach (string selection in one.Split('-')) {
+                    peopleSelections.Add(new PeopleSelection(int.Parse(selection)));
+                }
+            }
+
+            tovabbiJatekok.Text = "További játékok száma: " + peopleSelections.Count / 2;
+            specsf.Text += "\n1 - papír\n2 - olló";
+            Stats();
+        }
+
+        private void Selection_TextChanged(object sender, EventArgs e) {
+            eredmeny.Hide();
+
+            if (!IsBetween(firstSelection.Text, out int first) || !IsBetween(secondSelection.Text, out int second)) {
+                return;
+            }
+
+            PeopleSelection ps1 = new PeopleSelection(first);
+            PeopleSelection ps2 = new PeopleSelection(second);
+
+            peopleSelections.Add(ps1);
+            peopleSelections.Add(ps2);
+
+            PeopleSelection winner = ps1.CalculateWinner(ps2);
+
+            SaveToFile();
+
+            tovabbiJatekok.Text = "További játékok száma: " + peopleSelections.Count / 2;
+            eredmeny.Show();
+            eredmeny.Text = "Eredmény kódolva (0-döntetlen, 1-első nyert, 2-második nyert):";
+
+            if (winner == null) {
+                eredmeny.Text += "0";
+            } else {
+                eredmeny.Text += winner == ps1 ? "1" : "2";
+            }
+
+            Stats();
+        }
+
+        private bool IsBetween(string input, out int res) {
+            return int.TryParse(input, out res) && res >= 0 && res < 3;
+        }
+
+        private void SaveToFile() {
+            using (StreamWriter writer = File.CreateText("jatek.txt")) {
+                foreach (PeopleSelection one in peopleSelections) {
+                    writer.WriteLine($"{one.selection}-{2}");
+                }
+            }
+        }
+
+        private void Stats() {
+            int dontetlenek = 0, elsoJatekos = 0, masodikJatekos = 0;
+
+            for (int y = 0; y < peopleSelections.Count; y++) {
+                PeopleSelection first = peopleSelections[y];
+                PeopleSelection winner = first.CalculateWinner(peopleSelections[y++]);
+
+                if (winner == null) {
+                    dontetlenek++;
+                } else if (winner == first) {
+                    elsoJatekos++;
+                } else {
+                    masodikJatekos++;
+                }
+            }
+
+            string dontetlen = string.Format("{0,30}", $"Döntetlenek: {dontetlenek} db");
+            string elso = string.Format("{0,36}", $"Első játékos nyert: {elsoJatekos} db");
+            string masodik = string.Format("{0,39}", $"Második játékos nyert: {masodikJatekos} db");
+
+            statsText.Text = $"Statisztika\n{dontetlen}\n{elso}\n{masodik}";
+        }
+
+        private class PeopleSelection {
+
+            public readonly int selection;
+
+            public PeopleSelection(int selection) {
+                this.selection = selection;
+            }
+
+            public PeopleSelection CalculateWinner(PeopleSelection another) {
+                if (selection == another.selection) {
+                    return null;
+                }
+
+                if (selection == 0) {
+                    return another.selection == 1 ? another : this;
+                }
+
+                if (another.selection == 0) {
+                    return selection == 1 ? this : another;
+                }
+
+                if (selection == 1) {
+                    return another.selection == 2 ? another : this;
+                }
+
+                if (another.selection == 1) {
+                    return selection == 2 ? this : another;
+                }
+
+                if (selection == 2) {
+                    return another.selection == 1 ? this : another;
+                }
+
+                if (another.selection == 2) {
+                    return selection == 1 ? another : this;
+                }
+
+                return null;
+            }
+        }
+    }
+}
